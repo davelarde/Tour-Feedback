6a8972055b8a096d28d533b19a92ce58b4ac9c3b
davelard <davelard@cisco.com>
2023-09-15 16:16:47 -0400

    added sample data to the dynamo db table

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 var tableName = "TouristEmails"
 
 type TouristEmailItem struct {
 	TourDate      string   `json:"TourDate"`
-	TouristEmails []string `json:"TourstEmails"`
+	TouristEmails []string `json:"TouristEmails"`
 }
 
 func main() {
 	//initialize an aws session
-	AwsSess, err := session.NewSession(&aws.Config{
+	sess, err := session.NewSession(&aws.Config{
 
 		Region: aws.String("us-east-1"),
 	})
@@ -27,7 +28,7 @@ func main() {
 	}
 
 	//Create a DynamoDb client
-	svc := dynamodb.New(AwsSess)
+	svc := dynamodb.New(sess)
 	// define the dynamo db table schema
 	params := &dynamodb.CreateTableInput{
 		TableName: aws.String(tableName),
@@ -37,7 +38,7 @@ func main() {
 				KeyType:       aws.String("HASH"),
 			},
 		},
-		AttributeDefinitions: []*dynamodb.AttributeDefintion{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
 			{
 				AttributeName: aws.String("TourDate"),
 				AttributeType: aws.String("S"),
@@ -53,5 +54,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error creating table: %v", err)
 	}
+
+	fmt.Printf("Table %s created succesfully. \n", tableName)
+
+	//inserting dummy data to dynamodb
+	sampleData := []TouristEmailItem{
+		{
+			TourDate: "2023-09-01",
+			TouristEmails: []string{
+				"danielavelarde4@gmail.com",
+				"dani@dani.com",
+				"worldtraveler@gmail.com",
+				"tourst@test.com",
+			},
+		},
+		{
+			TourDate: "2023-09-02",
+			TouristEmails: []string{
+				"dani@dani.com",
+				"newintech@tech.com",
+				"danielavelarde4@gmail.com",
+				"serge@serge.com",
+			},
+		},
+	}
+
+	for _, item := range sampleData {
+		av, err := dynamodbattribute.MarshalMap(item)
+		if err != nil {
+			log.Fatalf("error Marshaling dynamodb item %v", err)
+		}
+
+		_, err = svc.PutItem(&dynamodb.PutItemInput{
+			TableName: aws.String(tableName),
+			Item:      av,
+		})
+		if err != nil {
+			log.Fatalf("error inserting item into DynamoDb %v", err)
+		}
+	}
+	fmt.Println("dummy data added into Dynamodb")
 
 }
