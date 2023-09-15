9a838ae00d9f8e092747f25df257956e61ef1578
davelard <davelard@cisco.com>
2023-09-15 15:56:30 -0400

    creating a table with dynamo db

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 var tableName = "TouristEmails"
@@ -25,5 +26,32 @@ func main() {
 		log.Fatalf("Error creating session: %v", err)
 	}
 
-	fmt.Println(AwsSess)
+	//Create a DynamoDb client
+	svc := dynamodb.New(AwsSess)
+	// define the dynamo db table schema
+	params := &dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("TourDate"),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		AttributeDefinitions: []*dynamodb.AttributeDefintion{
+			{
+				AttributeName: aws.String("TourDate"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	}
+	//Create a dynamo db table
+	_, err = svc.CreateTable(params)
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+
 }
