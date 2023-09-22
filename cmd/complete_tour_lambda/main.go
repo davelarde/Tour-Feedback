82b572765fe065341612ca12a93237db8e7bca65
davelard <davelard@cisco.com>
2023-09-22 15:02:52 -0400

    modularize it

--- cmd/complete_tour_lambda/main.go
+++ cmd/complete_tour_lambda/main.go
@@ -9,18 +9,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
 var (
 	dynamoDBClient *dynamodb.DynamoDB
 	tableName      = "TouristEmails"
 )
-
-type TouristEmailItem struct {
-	TourDate      string   `json:"TourDate"`
-	TouristEmails []string `json:"TouristEmails"`
-}
 
 func init() {
 	//initialize an aws session
@@ -85,88 +80,43 @@ func updateTourStatus(TourDate string, tourCompleted string) error {
 				S: aws.String(tourDate),
 			},
 		},
-		UpdateExpression: aws.String("Set TourStatus = :status")
-		ExpressionAttributeValues : map[string]*dynamodb.AttributeValue{
-			":status":{
+		UpdateExpression: aws.String("SET TourStatus = :status"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":status": {
 				S: aws.String(tourCompleted),
 			},
 		},
 	})
-	if err != nil{
+	if err != nil {
 		log.Print(err)
 		return err
 	}
 	return nil
 }
 
-
+func updateEmailAddresses(tourDate string, emailAddresses []string) error {
+	_, err := dynamoDBClient.UpdateItem(&dynamodb.UpdateItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"TourDate": {
+				S: aws.String(tourDate),
+			},
+		},
+		UpdateExpression: aws.String("SET TouristEmails = list_append(TouristEmails, :emails)"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":emails": {
+				SS: aws.StringSlice(emailAddresses),
+			},
+		},
+	})
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
+}
 
 func main() {
-
-	// define the dynamo db table schema
-	params := &dynamodb.CreateTableInput{
-		TableName: aws.String(tableName),
-		KeySchema: []*dynamodb.KeySchemaElement{
-			{
-				AttributeName: aws.String("TourDate"),
-				KeyType:       aws.String("HASH"),
-			},
-		},
-		AttributeDefinitions: []*dynamodb.AttributeDefinition{
-			{
-				AttributeName: aws.String("TourDate"),
-				AttributeType: aws.String("S"),
-			},
-		},
-		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-			ReadCapacityUnits:  aws.Int64(5),
-			WriteCapacityUnits: aws.Int64(5),
-		},
-	}
-	//Create a dynamo db table
-	_, err = svc.CreateTable(params)
-	if err != nil {
-		log.Fatalf("Error creating table: %v", err)
-	}
-
-	fmt.Printf("Table %s created succesfully. \n", tableName)
-
-	//inserting dummy data to dynamodb
-	sampleData := []TouristEmailItem{
-		{
-			TourDate: "2023-09-01",
-			TouristEmails: []string{
-				"danielavelarde4@gmail.com",
-				"dani@dani.com",
-				"worldtraveler@gmail.com",
-				"tourst@test.com",
-			},
-		},
-		{
-			TourDate: "2023-09-02",
-			TouristEmails: []string{
-				"dani@dani.com",
-				"newintech@tech.com",
-				"danielavelarde4@gmail.com",
-				"serge@serge.com",
-			},
-		},
-	}
-
-	for _, item := range sampleData {
-		av, err := dynamodbattribute.MarshalMap(item)
-		if err != nil {
-			log.Fatalf("error Marshaling dynamodb item %v", err)
-		}
-
-		_, err = svc.PutItem(&dynamodb.PutItemInput{
-			TableName: aws.String(tableName),
-			Item:      av,
-		})
-		if err != nil {
-			log.Fatalf("error inserting item into DynamoDb %v", err)
-		}
-	}
-	fmt.Println("dummy data added into Dynamodb")
+	lambda.Start(CompleteTourHandler)
 
 }
