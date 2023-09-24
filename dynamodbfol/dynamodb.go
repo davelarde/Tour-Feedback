994c9127afebbeae0140ef3ee806dcf485d4f798
davelard <davelard@cisco.com>
2023-09-24 19:51:23 -0400

    added lambda function and deployed it with terraform

--- dynamodbfol/dynamodb.go
+++ dynamodbfol/dynamodb.go
@@ -1,8 +1,54 @@
-package dynamodb
+package dynamodbfol
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
 var TableName = "TouristEmails"
 
 type TouristEmailItem struct {
 	TourDate      string   `json:"TourDate"`
 	TouristEmails []string `json:"TouristEmails"`
+}
+
+func init() {
+	//initialize an aws session
+	sess, err := session.NewSession(&aws.Config{
+
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		log.Fatalf("Error creating session: %v", err)
+	}
+
+	//Create a DynamoDb client
+	DynamoDBClient = dynamodb.New(sess)
+}
+
+var DynamoDBClient *dynamodb.DynamoDB
+
+func UpdateEmailAddresses(tourDate string, emailAddresses []string) error {
+	_, err := DynamoDBClient.UpdateItem(&dynamodb.UpdateItemInput{
+		TableName: aws.String(TableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"TourDate": {
+				S: aws.String(tourDate),
+			},
+		},
+		UpdateExpression: aws.String("SET TouristEmails = list_append(TouristEmails, :emails)"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":emails": {
+				SS: aws.StringSlice(emailAddresses),
+			},
+		},
+	})
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
 }
